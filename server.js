d99758b3ebd7b8e80e49d1f5ce9ce099d777b0f7
Dea Limoni <dea.limoni@student.uni-pr.edu>
2024-11-08 15:11:52 +0100

    update server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const net = require('net');
+const fs = require('fs'); 
+const { exec } = require("child_process"); 
+
+// Krijimi i serverit dhe mesazhi nga i cili bëhet lidhja e adresës dhe portit
+const server = net.createServer((socket) => { 
+    console.log(  
+        'Connection from',
+        socket.remoteAddress,
+        'port',
+        socket.remotePort
+    );
+   
