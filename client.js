241012c4a464bb19b67b6e74de99e405ba25d53e
ElisaBerisha <elisaberisha888@gmail.com>
2024-11-13 13:53:30 -0800

    Update client.js

---
+++ client.js
@@ -0,0 +1,30 @@
+const net = require('net');
+const readline = require('readline');
+
+// Zëvendësoni 'SERVER_IP' me adresën aktuale të IP serverit tuaj
+const host = '192.168.0.10';
+var port = 837; 
+
+const client = new net.Socket();
+
+
+client.connect(port, host, () => {
+  console.log('U lidhet me server');
+  console.log("Shkruani login(ne lowercase) per te pasur casje per read, write ose execute");
+});
+
+client.on('data', (data) => {
+  console.log(data.toString('utf-8'));
+});
+
+const rl = readline.createInterface({
+  input: process.stdin
+});
+
+rl.on('line', (line) => {
+  client.write(`${line}\n`);
+});
+
+rl.on('close', () => {
+  client.end();
+});
