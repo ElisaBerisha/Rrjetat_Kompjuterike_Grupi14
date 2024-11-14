46afa311bcb123abf9a9c7f94248ada3a6183b3f
dioni5 <151408301+dioni5@users.noreply.github.com>
2024-11-14 22:19:12 +0100

    uptade

--- client.js
+++ client.js
@@ -12,3 +12,19 @@ client.connect(port, host, () => {
   console.log('Jeni lidhur me server');
   console.log("Shkruani login: ");
 });
+
+client.on('data', (data) => {
+    console.log(data.toString('utf-8'));
+  });
+  
+  const rl = readline.createInterface({
+    input: process.stdin
+  });
+  
+  rl.on('line', (line) => {
+    client.write(`${line}\n`);
+  });
+  
+  rl.on('close', () => {
+    client.end();
+  });
