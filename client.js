1619084fccff2b1cb56a2dd50feff24217520862
ElisaBerisha <elisaberisha888@gmail.com>
2024-11-16 17:38:48 -0800

    Update client.js

--- client.js
+++ client.js
@@ -2,7 +2,7 @@ const net = require('net');
 const readline = require('readline');
 
 // Zëvendësoni 'SERVER_IP' me adresën aktuale të IP serverit tuaj
-const host = '192.168.1.7';
+const host = '192.168.0.7';
 var port = 838; 
 
 const client = new net.Socket();
